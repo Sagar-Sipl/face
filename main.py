9e9423fe5f9b873ca0261195992ddabb19480d1c
Sagar-Sipl <159750833+Sagar-Sipl@users.noreply.github.com>
2024-04-08 15:51:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import json
-import cv2
-import base64
-import numpy as np
-from face_recognition_system import register
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+import json
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -17,28 +13,12 @@ def index():
 def handle_message(message):
     # print('Received message: ' + message)
     try:
-        print('Received message: ' , type(message))
-        if message=="stop":
-           socketio.emit("message",json.dumps( {"status": "False", "message": "stop"}))
-        else:
-            #    async def send_response(message):
-                response =  recognize_face(message)
-                print("done")
-                socketio.emit("message", json.dumps(response))
+            socketio.emit("message", json.dumps("Hi user"))
           
     except Exception as e:
         print(f"WebSocket Error: {str(e)}")
         
 
-def bs64_to_frame(bs64):
-        if bs64:
-            decoded_bytes = base64.b64decode(bs64)
-            decoded_image = np.frombuffer(decoded_bytes, dtype=np.uint8)
-            decoded_image = cv2.imdecode(decoded_image, flags=cv2.IMREAD_COLOR)
-            return decoded_image
-        else:
-            return 'Unable to process image'
-        
 
 
 
@@ -51,31 +31,6 @@ def testing():
         return json.dumps("")
 
 
-
-
-def bytes_to_frame(byte_array):
-    nparr = np.frombuffer(byte_array, np.uint8)
-    
-    frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    
-    return frame
-
-async def recv_and_display(frame):
-    cv2.imshow("Video Stream", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        pass
-
-def recognize_face(message):
-    try:
-        print("type(message)",type(message))
-        by= bytes_to_frame(message)
-
-        data =register.getFace(by)
-        print("data",type(data))
-        
-        return data
-    except Exception as e:
-        return {"status": False, "message": str(e)}
 
 
 
